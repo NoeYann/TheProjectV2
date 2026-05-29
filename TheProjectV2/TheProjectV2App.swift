3dd45d71490c9c514b9d1dc1d31050f4de523ed6
Noe-Yann <noeyann.ck@gmail.com>
2026-05-29 14:04:47 +0200

    Initial Commit

---
+++ TheProjectV2/TheProjectV2App.swift
@@ -0,0 +1,17 @@
+//
+//  TheProjectV2App.swift
+//  TheProjectV2
+//
+//  Created by Noe-Yann Chaffard--Kowalewski on 29/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct TheProjectV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TheProjectV2Tests/TheProjectV2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  TheProjectV2Tests.swift
+//  TheProjectV2Tests
+//
+//  Created by Noe-Yann Chaffard--Kowalewski on 29/05/2026.
+//
+
+import Testing
+@testable import TheProjectV2
+
+struct TheProjectV2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
